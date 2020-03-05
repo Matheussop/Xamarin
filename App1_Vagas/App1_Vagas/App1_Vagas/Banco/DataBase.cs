f33a9f499469e0d9f1ad5694205bb933799a00ed
Matheus Luiz <matheusluiz98@hotmail.com>
2020-03-05 16:30:29 -0300

    Fim do projeto CINEMIX

---
+++ App1_Vagas/App1_Vagas/App1_Vagas/Banco/DataBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using App1_Vagas.Modelos;
+
+namespace App1_Vagas.Banco
+{
+    class DataBase
+    {
+        private SQLiteConnection _conexao;
+        
+        public DataBase()
+        {
+            _conexao = new SQLiteConnection();
+        }
+
+        public void Cadastro(Vagas vaga)
+        {
+
+        }
+
+        public List<Vagas> Consultar()
+        {
+            return null;
+        }
+        public Vagas ObterVagaPorId(int id)
+        {
+            return null;
+        }
+        public void Atualizacao(Vagas vaga)
+        {
+
+        }
+        public void Exclusao(int id)
+        {
+
+        }
+    }
+}
---
+++ App1_Vagas/App1_Vagas/App1_Vagas/Modelos/Vagas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1_Vagas.Modelos
+{
+    public class Vagas
+    {
+        public string NomeVaga { get; set; }
+        public short Quantidade { get; set; }
+        public string Cidade { get; set; }
+        public double salario { get; set; }
+        public string descricao { get; set; }
+        public string TipoContratacao{ get; set; }
+        public string Telefone{ get; set; }
+        public string Email { get; set; }
+    }
+}
